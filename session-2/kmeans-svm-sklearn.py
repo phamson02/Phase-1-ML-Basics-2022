29c6b2792565dbaff83879536c4d1565a82ca53e
phamson02 <phamtienson02@gmail.com>
2022-04-01 08:08:46 +0700

    ss2: kmeans-svm-sklearn: change load_data func, update behaviours of svm classifying func and compute accuracy func

--- session-2/kmeans-svm-sklearn.py
+++ session-2/kmeans-svm-sklearn.py
@@ -5,9 +5,6 @@ from sklearn.svm import LinearSVC, SVC
 PATH = '../data/20news-bydate'
 
 def load_data(data_path):
-    with open(f'{PATH}/words-idfs.txt') as f:
-        vocab_size = sum(1 for _ in f)
-
     def sparse_to_dense(sparse_r_d, vocab_size):
         r_d = np.zeros(vocab_size)
         indices_tfidf = sparse_r_d.split()
@@ -17,29 +14,32 @@ def load_data(data_path):
             r_d[index] = tfidf
         return r_d
 
-    data = np.loadtxt(
-        fname=data_path,
-        delimiter='<fff>',
-        usecols=2,
-        converters={
-            2: lambda x: sparse_to_dense(str(x)[2:-1], vocab_size)
-        },
-    )
+    with open(f'{PATH}/words-idfs.txt') as f:
+        vocab_size = sum(1 for _ in f)
 
-    labels = np.loadtxt(
-        fname=data_path,
-        delimiter='<fff>',
-        usecols=0,
-    )
+    with open(f'{PATH}/{data_path}') as f:
+        data_size = sum(1 for _ in f)
+
+    data = np.empty((data_size, vocab_size))
+    labels = np.empty(data_size)
+    with open(f'{PATH}/{data_path}') as f:
+        for idx, line in enumerate(f):
+            features = line.split('<fff>')
+            label = int(features[0])    
+            r_d = sparse_to_dense(features[2], vocab_size)
+            data[idx] = r_d
+            labels[idx] = label
 
     return data, labels
 
-def compute_accuracy(y_pred, y_test):
+def compute_accuracy(classifier):
+    X_test, y_test = load_data(data_path=f'20news-test-tf-idf.txt')
+    y_pred = classifier.predict(X_test)
     accuracy = np.mean(y_pred == y_test)
     return accuracy
 
 def clustering_with_KMeans():
-    X, _ = load_data(data_path=f'{PATH}/20news-full-tf-idf.txt')
+    X, _ = load_data(data_path=f'20news-full-tf-idf.txt')
     kmeans = KMeans(
         n_clusters=20,
         init='random',
@@ -52,7 +52,7 @@ def clustering_with_KMeans():
     return kmeans
 
 def classifying_with_linear_SVM():
-    X_train, y_train = load_data(data_path=f'{PATH}/20news-train-tf-idf.txt')
+    X_train, y_train = load_data(data_path=f'20news-train-tf-idf.txt')
     classifier = LinearSVC(
         C=10.0,
         tol=1e-3,
@@ -60,16 +60,10 @@ def classifying_with_linear_SVM():
     )
     classifier.fit(X_train, y_train)
 
-    X_test, y_test = load_data(data_path=f'{PATH}/20news-test-tf-idf.txt')
-    y_pred = classifier.predict(X_test)
-    accuracy = compute_accuracy(y_pred, y_test)
-
-    return accuracy
+    return classifier
 
 def classifying_with_kernel_SVM():
-    X_train, y_train = load_data(data_path=f'{PATH}/20news-train-tf-idf.txt')
-    print(X_train)
-    print(y_train)
+    X_train, y_train = load_data(data_path=f'20news-train-tf-idf.txt')
     classifier = SVC(
         C=50.0,
         kernel='rbf',
@@ -79,21 +73,11 @@ def classifying_with_kernel_SVM():
     )
     classifier.fit(X_train, y_train)
 
-
-    X_test, y_test = load_data(data_path=f'{PATH}/20news-test-tf-idf.txt')
-    y_pred = classifier.predict(X_test)
-    accuracy = compute_accuracy(y_pred, y_test)
-
-    return accuracy
+    return classifier
 
 
 if __name__ == '__main__':
-    # # accuracy_with_KMeans = clustering_with_KMeans()
-    # accuracy_with_linear_SMSVC = classifying_with_linear_SVM()
-    # accuracy_with_kernel_SMSVC = classifying_with_kernel_SVM()
-    # # print(f'accuracy_with_KMeans: {accuracy_with_KMeans}')
-    # print(f'accuracy_with_linear_SMSVC: {accuracy_with_linear_SMSVC}')
-    # print(f'accuracy_with_kernel_SMSVC: {accuracy_with_kernel_SMSVC}')
-    data, labels = load_data(data_path=f'{PATH}/20news-full-tf-idf.txt')
-    print(data[0,-2])
-    
+    linear_SMSVC = classifying_with_linear_SVM()
+    kernel_SMSVC = classifying_with_kernel_SVM()
+    print(f'accuracy_with_linear_SMSVC: {compute_accuracy(linear_SMSVC)}')
+    print(f'accuracy_with_kernel_SMSVC: {compute_accuracy(kernel_SMSVC)}')
